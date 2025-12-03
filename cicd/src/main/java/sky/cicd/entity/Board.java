9a296916551957edd897c0c63a03ac42e4815305
goldrex123 <goldrex123@naver.com>
2025-12-03 13:57:06 +0900

    도커 파일 생성 yml 파일 설정

--- cicd/src/main/java/sky/cicd/entity/Board.java
+++ cicd/src/main/java/sky/cicd/entity/Board.java
@@ -1,9 +1,6 @@
 package sky.cicd.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,7 +11,7 @@ import sky.cicd.common.domain.BaseTimeEntity;
 @Getter
 public class Board extends BaseTimeEntity {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "board_id")
     private Long id;
 
