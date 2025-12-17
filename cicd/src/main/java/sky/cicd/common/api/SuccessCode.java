7ab58af9f336128135ae43649cf50b08e47dd66a
goldrex123 <goldrex123@naver.com>
2025-12-17 11:43:28 +0900

    cicd 테스트2

--- cicd/src/main/java/sky/cicd/common/api/SuccessCode.java
+++ cicd/src/main/java/sky/cicd/common/api/SuccessCode.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum SuccessCode {
 
-    SUCCESS("S001", "요청이 성공했습니다아"),
+    SUCCESS("S001", "요청이 성공했습니다아!"),
     CREATED("S002", "정상적으로 생성되었습니다"),
     UPDATED("S003", "정상적으로 수정되었습니다"),
     DELETED("S004", "정상적으로 삭제되었습니다");
