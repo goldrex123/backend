bd02d036bf4311f7df7274c4bfe58c513d0de053
goldrex123 <goldrex123@naver.com>
2026-01-16 17:38:20 +0900

    test commit

--- transaction-lock/src/main/java/sky/spring/transaction_lock/TransactionLockApplication.java
+++ transaction-lock/src/main/java/sky/spring/transaction_lock/TransactionLockApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TransactionLockApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello World");
 		SpringApplication.run(TransactionLockApplication.class, args);
 	}
 
