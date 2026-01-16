1a6eebacd49427eaf300b81e49ce024b6fa51f63
goldrex123 <goldrex123@naver.com>
2026-01-16 17:34:33 +0900

    transaction lock dir 생성

---
+++ transaction-lock/src/main/java/sky/spring/transaction_lock/TransactionLockApplication.java
@@ -0,0 +1,13 @@
+package sky.spring.transaction_lock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionLockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionLockApplication.class, args);
+	}
+
+}
---
+++ transaction-lock/src/test/java/sky/spring/transaction_lock/TransactionLockApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.spring.transaction_lock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionLockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
