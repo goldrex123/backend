d8d2abdca32ef87160bd1e81fd616d4afab52725
goldrex123 <goldrex123@naver.com>
2026-02-02 14:38:29 +0900

    [pg] feat: Project settings

---
+++ pg/src/test/java/sky/spring/pg/PgApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.spring.pg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
